ff63a746e54aa51cdd1607e4b66d371cd7a929b8
Enzo Bucchi <enzo.bucchi@outlook.com>
2022-04-27 18:58:51 -0300

    Implementação hr-worker

---
+++ hr-worker/src/main/java/com/bucchi/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.bucchi.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bucchi.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+}
