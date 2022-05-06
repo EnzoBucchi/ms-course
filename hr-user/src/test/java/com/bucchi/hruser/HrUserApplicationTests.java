dc8a642327865c59b8d8e6526f5cde35c40f647d
Enzo Bucchi <enzo.bucchi@outlook.com>
2022-05-06 15:46:40 -0300

    Projeto hr-user criado

---
+++ hr-user/src/test/java/com/bucchi/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bucchi.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
